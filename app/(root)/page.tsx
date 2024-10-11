43b0190a78083e9e6862a030224c5583ea890221
Murtaza Narwarwala <murtazanarwar@gmail.com>
2024-10-11 09:11:06 +0530

    store modal creation complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 export default function setupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if( !isOpen ){
+      onOpen();
+    }
+  },[isOpen, onOpen]);
+
   return (
-    <div className="p-4 flex items-start justify-end">
-      <UserButton afterSignOutUrl="/" />
+    <div className="p-4">
+      Root Page
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
-
 import { ClerkProvider } from '@clerk/nextjs'
+
+import { ModalProvider } from "@/provider/modal-provider";
+
+import "./globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +33,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           >
+            <ModalProvider />
           {children}
         </body>
       </html>
