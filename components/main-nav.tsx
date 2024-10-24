1ac77c55cd7239130e1e9c8bdc9780d2fcbc6c3d
Murtaza Narwarwala <murtazanarwar@gmail.com>
2024-10-24 19:57:41 +0530

    attempt to make mobile responsive navbar

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -3,70 +3,89 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
 export function MainNav({
-    className,
-    // ...props
-}: React.HTMLAttributes<HTMLElement> ) {
-    const pathName = usePathname();
-    const params = useParams();
+  className,
+}: React.HTMLAttributes<HTMLElement>) {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathName = usePathname();
+  const params = useParams();
 
-    const routes = [
-        {
-          href: `/${params.storeId}`,
-          label: 'Overview',
-          active: pathName === `/${params.storeId}`,
-        },
-        {
-          href: `/${params.storeId}/billboards`,
-          label: 'Billboards',
-          active: pathName === `/${params.storeId}/billboards`,
-        },
-        {
-          href: `/${params.storeId}/categories`,
-          label: 'Categories',
-          active: pathName === `/${params.storeId}/categories`,
-        },
-        {
-          href: `/${params.storeId}/sizes`,
-          label: 'Sizes',
-          active: pathName === `/${params.storeId}/sizes`,
-        },
-        {
-          href: `/${params.storeId}/colors`,
-          label: 'Colors',
-          active: pathName === `/${params.storeId}/colors`,
-        },
-        {
-          href: `/${params.storeId}/products`,
-          label: 'Products',
-          active: pathName === `/${params.storeId}/products`,
-        },
-        {
-          href: `/${params.storeId}/orders`,
-          label: 'Orders',
-          active: pathName === `/${params.storeId}/orders`,
-        },
-        {
-          href: `/${params.storeId}/settings`,
-          label: 'Settings',
-          active: pathName === `/${params.storeId}/settings`,
-        },
-    ];  
-    return (
-        <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
-            {routes.map((route) => (
-                <Link
-                    key={route.href}
-                    href={route.href}
-                    className={cn(
-                        "text-sm font-medium transition-colors hover:text-primary",
-                        route.active ? "text-black dark:text-white" : "text-muted-foreground"
-                    )}
-                >
-                    {route.label} {/* Added the label here */}
-                </Link>
-            ))}
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const routes = [
+    {
+      href: `/${params.storeId}`,
+      label: 'Overview',
+      active: pathName === `/${params.storeId}`,
+    },
+    {
+      href: `/${params.storeId}/billboards`,
+      label: 'Billboards',
+      active: pathName === `/${params.storeId}/billboards`,
+    },
+    {
+      href: `/${params.storeId}/categories`,
+      label: 'Categories',
+      active: pathName === `/${params.storeId}/categories`,
+    },
+    {
+      href: `/${params.storeId}/sizes`,
+      label: 'Sizes',
+      active: pathName === `/${params.storeId}/sizes`,
+    },
+    {
+      href: `/${params.storeId}/colors`,
+      label: 'Colors',
+      active: pathName === `/${params.storeId}/colors`,
+    },
+    {
+      href: `/${params.storeId}/products`,
+      label: 'Products',
+      active: pathName === `/${params.storeId}/products`,
+    },
+    {
+      href: `/${params.storeId}/orders`,
+      label: 'Orders',
+      active: pathName === `/${params.storeId}/orders`,
+    },
+    {
+      href: `/${params.storeId}/settings`,
+      label: 'Settings',
+      active: pathName === `/${params.storeId}/settings`,
+    },
+  ];
+
+  return (
+    <div>
+      <div className={`absolute z-50 sm:static bg-white sm:bg-transparent sm:min-h-0 left-0 sm:left-auto ${isOpen ? 'top-[9%] mb-4' : 'top-[-100%]'} sm:top-auto w-full sm:w-auto flex sm:flex-none items-center sm:items-start px-5 pt-4 sm:px-0`}>
+        <nav className={cn("flex flex-col sm:flex-row sm:items-center sm:space-x-4 lg:space-x-6 gap-8 sm:gap-0", className)}>
+          {routes.map((route) => (
+            <Link onClick={toggleNavbar}
+              key={route.href}
+              href={route.href}
+              className={cn(
+                "text-sm font-medium transition-colors hover:text-primary",
+                route.active ? "text-black dark:text-white" : "text-muted-foreground"
+              )}
+            >
+              {route.label}
+            </Link>
+          ))}
         </nav>
-    );
-};
+      </div>
+
+      <div className="flex items-center absolute right-[8%] top-5 cursor-pointer sm:hidden">
+        {!isOpen ? (
+          <Menu onClick={toggleNavbar} />
+        ) : (
+          <X onClick={toggleNavbar} />
+        )}
+      </div>
+    </div>
+  );
+}
